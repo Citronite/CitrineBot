19b8c701b35f402fd26efd21f47780cd4648d555
Quantomistro3178 <m.saifi3178@gmail.com>
2019-02-23 14:58:07 +0800

    add message event listener

---
+++ src/Events/message.ts
@@ -0,0 +1,11 @@
+import { Message } from 'discord.js';
+import { CitrineClient } from '../Structures/CitrineClient';
+
+export function listener(client: CitrineClient, message: Message): void {
+	const { CmdHandler, settings } = client;
+	
+	const config = settings.getGuild(message.guild) || settings.setGuild(message.guild);
+	if (config.deleteCmdCalls) message.delete();
+
+	CmdHandler.processCmd(message, config); // CmdHandler.processCmd() should ONLY check perms, and then execute cmd. no other side effects.
+}
