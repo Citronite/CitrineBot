363286c8e8ccbdac497f4a3c393d6d44a321a69c
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-07 15:46:33 +0800

    PermHandler WIP

--- src/Structures/PermHandler.ts
+++ src/Structures/PermHandler.ts
@@ -1,12 +1,29 @@
-import { Command } from 'typings';
 import { Message } from 'discord.js';
+import { CommandError, ErrorTypes } from './CommandError';
+import { IGuildConfig, Command } from 'typings';
 
 export class PermHandler {
 	constructor() {
 		throw new Error('This class may not be instantiated!');
 	}
 
-	public static checkCustomFilters(cmd: Command, message: Message): Error | void {
-		return false;
+	public static checkCustomFilters(cmd: Command, message: Message, client): Error | void {
+		const { config: globalConfig } = client.settings;
+		const config: IGuildConfig = client.settings.getGuild(message.guild.id);
+		const errors: string[] = [];
+
+		if (message.author.id === globalConfig.owner) return;
+
+		if (globalConfig.disabledUsers.has(message.author.id)) errors.push('Disabled User [Global]');
+
+		if (config.disabledUsers.has(message.author.id)) errors.push('Disabled User [Local]');
+
+		if (config.disabledChannels.has(message.channel.id)) errors.push('Disabled Channel');
+
+		if (globalConfig.disabledCommands.has(cmd.name)) errors.push('Disabled Command [Global]');
+
+		if (config.disabledCommands.has(cmd.name)) errors.push('Disabled Command [Local]');
+
+		if (errors.length) return new CommandError(cmd, ErrorTypes.FAILED_CUSTOM_FILTERS, errors);
 	}
 }
