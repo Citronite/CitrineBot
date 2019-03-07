891938c226605bd1464cf28f2e9eb75e118b65dd
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-07 15:45:52 +0800

    Fix some imports

--- src/Structures/CitrineUtils.ts
+++ src/Structures/CitrineUtils.ts
@@ -1,18 +1,18 @@
-import { djsUtils } from '../Utils/djsUtils';
-import { jsUtils } from '../Utils/jsUtils';
+import { DjsUtils } from '../Utils/DjsUtils';
+import { JsUtils } from '../Utils/JsUtils';
 import { CmdHandler } from './CmdHandler';
 import { PermHandler } from './PermHandler';
 
 export class CitrineUtils {
 	public CmdHandler: CmdHandler;
 	public PermHandler: PermHandler;
-	public djs: djsUtils;
-	public js: jsUtils;
+	public djs: DjsUtils;
+	public js: JsUtils;
 
 	constructor() {
 		this.CmdHandler = CmdHandler;
 		this.PermHandler = PermHandler;
-		this.djs = djsUtils;
-		this.js = jsUtils;
+		this.djs = DjsUtils;
+		this.js = JsUtils;
 	}
 }
