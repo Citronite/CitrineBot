157d7e2dc5842d0512c90f22f6e5718343bf75de
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-10 14:39:59 +0800

    Oops

--- src/Structures/CitrineClient.ts
+++ src/Structures/CitrineClient.ts
@@ -17,7 +17,24 @@ export class CitrineClient extends Client {
 		return 1 > 0;
 	}
 
-	public launch(TOKEN: string): void {
+	public async loadModule(): Promise<boolean> {
+		return Promise.reject(false);
+	}
+
+	public async unloadModule(): Promise<boolean> {
+		return Promise.reject(false);
+	}
+
+	public async clearCachedModule(): Promise<boolean> {
+		return Promise.reject(false);
+	}
+
+	public async reloadModule(): Promise<boolean> {
+		return Promise.reject(false);
+	}
+
+	public async launch(TOKEN: string): Promise<boolean> {
 		// Starts bot!
+		return Promise.reject(false);
 	}
 }
