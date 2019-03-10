e312d4377478a4cd86ff93c33f7408cc8c0b1ffe
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-10 14:47:11 +0800

    :v

--- src/Events/message.ts
+++ src/Events/message.ts
@@ -2,7 +2,8 @@ import { Message } from 'discord.js';
 import { CitrineClient } from '../Structures/CitrineClient';
 
 export function listener(client: CitrineClient, message: Message): void {
-	const { CmdHandler, settings } = client;
+	const { settings } = client;
+	const { CmdHandler } = client.utils;
 	
 	const config = settings.getGuild(message.guild) || settings.setGuild(message.guild);
 	if (config.deleteCmdCalls) message.delete();
