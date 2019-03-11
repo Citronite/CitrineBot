8a8fbb27784924e8031554b68d576731540a04f8
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-11 18:57:15 +0800

    not sure what exactly i changed....i think my editor is acting up

--- src/Events/message.ts
+++ src/Events/message.ts
@@ -4,7 +4,7 @@ import { CitrineClient } from '../Structures/CitrineClient';
 export function listener(client: CitrineClient, message: Message): void {
 	const { settings } = client;
 	const { CmdHandler } = client.utils;
-	
+
 	const config = settings.getGuild(message.guild) || settings.setGuild(message.guild);
 	if (config.deleteCmdCalls) message.delete();
 
