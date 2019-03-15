719074a62904381798c64f646230407b51475a1c
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-15 22:59:51 +0800

    Added Constants.ts

---
+++ src/Utils/Constants.ts
@@ -0,0 +1,16 @@
+export const Colors: any = {
+	RED: 0xCB4154,
+	GREEN: 0x00FF40,
+	BLUE: 0x87CEFA,
+	YELLOW: 0xFFF600,
+	CANARY: 0xFFEF00,
+	ORANGE: 0xFF6600,
+	TURQUOISE: 0x40E0D0,
+	PURPLE: 0xCC397B,
+	FUCHSIA: 0xC154C1,
+	GOLD: 0xEFCC00,
+	DARK: 10, // Discord Dark Mode color
+	LIGHT: 11, // Discord Light Mode color
+	CYAN: 0x00FFFF,
+	PINK: 0xF77FBE
+};
