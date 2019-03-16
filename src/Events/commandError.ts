20a16a14872f3dfec609845f0cf730cb03b4840b
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-17 00:03:48 +0800

    commandError event just sends an error embed. Might add better handling later

--- src/Events/commandError.ts
+++ src/Events/commandError.ts
@@ -1,7 +1,10 @@
+import { Context } from '../Utils/Context';
+import { CommandError } from '../Structures/ErrorStructs/CommandError';
+
 module.exports = {
 	name: 'commandError',
 	maxListeners: '1',
-	listener: () => {
-		return;
+	listener: (ctx: Context, error: CommandError): void => {
+		ctx.send(error.toEmbed());
 	}
 };
