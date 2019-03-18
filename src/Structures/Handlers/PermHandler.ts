7db3684632b8aad8de3c4556f2a3345fd2b4488b
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-18 23:54:09 +0800

    PermHandler.checkCustomFilter is is now an asyn function

--- src/Structures/Handlers/PermHandler.ts
+++ src/Structures/Handlers/PermHandler.ts
@@ -1,6 +1,5 @@
-import { IGuildConfig } from 'typings';
+import { GuildConfig } from '../../Utils/GuildConfig';
 import { CitrineClient } from '../CitrineClient';
-import { CommandError } from '../ErrorStructs/CommandError';
 import { BaseError } from '../ErrorStructs/BaseError';
 import { ErrorCodes } from '../ErrorStructs/ErrorCodes';
 import { Command } from '../CommandStructs/AbstractCommand';
@@ -19,29 +18,31 @@ export class PermHandler {
 		throw new Error('This class may not be instantiated with new!');
 	}
 
-	public static checkCustomFilters(cmd: Command, message: Message, client: CitrineClient): void | CommandError {
+	public static async checkCustomFilters(cmd: Command, message: Message, client: CitrineClient): Promise<boolean> {
 		const { globalConfig } = client.settings;
-		const config: IGuildConfig | null = client.settings.getGuild(message.guild.id);
-		if (!config) throw new BaseError(ErrorCodes.NOT_FOUND, [`GuildConfig not found (id: ${message.guild.id})`]);
+		try {
+			const config: GuildConfig = await client.settings.getGuild(message.guild.id);
+			const errors = [];
 
-		const errors = [];
+			if (message.author.id === globalConfig.owner) return Promise.resolve(true);
 
-		if (message.author.id === globalConfig.owner) return;
+			if (globalConfig.disabledUsers.has(message.author.id)) errors.push('Disabled User [Global]');
 
-		if (globalConfig.disabledUsers.has(message.author.id)) errors.push('Disabled User [Global]');
+			if (config.disabledUsers.has(message.author.id)) errors.push('Disabled User [Local]');
 
-		if (config.disabledUsers.has(message.author.id)) errors.push('Disabled User [Local]');
+			if (config.disabledChannels.has(message.channel.id)) errors.push('Disabled Channel');
 
-		if (config.disabledChannels.has(message.channel.id)) errors.push('Disabled Channel');
+			if (globalConfig.disabledCommands.has(cmd.name)) errors.push('Disabled Command [Global]');
 
-		if (globalConfig.disabledCommands.has(cmd.name)) errors.push('Disabled Command [Global]');
+			if (config.disabledCommands.has(cmd.name)) errors.push('Disabled Command [Local]');
 
-		if (config.disabledCommands.has(cmd.name)) errors.push('Disabled Command [Local]');
-
-		if (errors.length) throw new CommandError(cmd, ErrorCodes.FAILED_CUSTOM_FILTERS, errors);
+			return errors.length ? Promise.reject(new BaseError(ErrorCodes.FAILED_CUSTOM_FILTERS, errors)) : Promise.resolve(true);
+		} catch (err) {
+			return Promise.reject(err);
+		}
 	}
 
-	public static checkDiscordPerms(channel: TextChannel, member: GuildMember, perms: PermissionResolvable, checkAdmin: boolean = true): void | Error {
+	public static checkDiscordPerms(channel: TextChannel, member: GuildMember, perms: PermissionResolvable, checkAdmin: boolean = true): void | BaseError {
 		const memberPerms = channel.memberPermissions(member);
 		if (memberPerms === null) throw new BaseError(ErrorCodes.NOT_FOUND, [`Member permissions not found (id: ${member.id})`]);
 
