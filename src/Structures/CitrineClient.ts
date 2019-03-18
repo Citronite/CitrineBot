076b534b150ee77490b964fa8dccb00e1d885957
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-18 23:46:23 +0800

    Add logger property to CitrineClient. Also, move CmdHandler and PermHandler outside CitrineUtils

--- src/Structures/CitrineClient.ts
+++ src/Structures/CitrineClient.ts
@@ -3,18 +3,28 @@ import { CitrineLogger } from './CitrineStructs/CitrineLogger';
 import { CitrineUtils } from './CitrineStructs/CitrineUtils';
 import { CitrineDB } from './CitrineStructs/CitrineDB';
 import { Client, ClientOptions } from 'discord.js';
+import { CmdHandler } from './Handlers/CmdHandler';
+import { PermHandler } from './Handlers/PermHandler';
 
 export class CitrineClient extends Client {
 	public readonly settings: CitrineSettings;
+	public readonly logger: CitrineLogger;
 	public readonly utils: CitrineUtils;
 	public readonly db: CitrineDB;
+
+	public readonly cmdHandler: CmdHandler;
+	public readonly permHandler: PermHandler;
 
 	constructor(options: ClientOptions) {
 		super(options);
 
 		this.settings = new CitrineSettings(this);
+		this.logger = new CitrineLogger();
 		this.utils = new CitrineUtils();
 		this.db = new CitrineDB();
+
+		this.cmdHandler = CmdHandler;
+		this.permHandler = PermHandler;
 	}
 
 	public initModules(defaultModules: string[]): boolean {
