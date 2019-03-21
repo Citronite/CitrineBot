d2d00a12557f673b067b3f3d4e9f90cc65e27594
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-22 02:42:13 +0800

    [ErrorStructs] Better input validation, plus toString returns a codeblock by default now

--- src/Structures/ErrorStructs/BaseError.ts
+++ src/Structures/ErrorStructs/BaseError.ts
@@ -1,6 +1,7 @@
 import { RichEmbed } from 'discord.js';
 import { QuickEmbed } from '../../Utils/QuickEmbed';
 import { ErrorCodes } from './ErrorCodes';
+import { ExceptionParser } from './ExceptionParser';
 
 export class BaseError extends Error {
 	public readonly code: number;
@@ -8,19 +9,20 @@ export class BaseError extends Error {
 	public readonly name: string;
 	public readonly errors: string[];
 	public readonly message: string;
+
 	constructor(code: number, errors: string[]) {
 		super();
-		this.code = code;
-		this.type = ErrorCodes[code];
+		this.code = Object.values(ErrorCodes).includes(code) ? code : 999;
+		this.type = Object.keys(ErrorCodes).find(val => ErrorCodes[val] === code) || 'UNKNOWN_ERROR';
 		this.name = `${this.type}:${this.code}`;
-		this.errors = errors;
+		this.errors = errors || ExceptionParser.getDefaultMessages()[this.code];
 		this.message = `Error(s):\n\t${errors.join('\n')}`;
 	}
 
 	public toString(): string {
-		const top = `⛔ **${this.name}:${this.code}** ⛔`;
+		const top = `⛔ ${this.name} ⛔`;
 		const msg = this.message;
-		return `${top}\n\n${msg}`;
+		return `\`\`\`\n${top}\n\n${msg}\n\`\`\``;
 	}
 
 	public toEmbed(): RichEmbed {
