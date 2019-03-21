fa2f1fb8bdf0098f0a482ab4d0b9588c05a1dfc6
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-22 02:39:30 +0800

    [CitrineStructs] add the formatter class to CitrineUtils

--- src/Structures/CitrineStructs/CitrineUtils.ts
+++ src/Structures/CitrineStructs/CitrineUtils.ts
@@ -1,12 +1,15 @@
 import { DjsUtils } from '../../Utils/DjsUtils';
 import { JsUtils } from '../../Utils/JsUtils';
+import { Formatter } from '../../Utils/Formatter';
 
 export class CitrineUtils {
-	public djs: DjsUtils;
-	public js: JsUtils;
+	public readonly djs: DjsUtils;
+	public readonly js: JsUtils;
+	public readonly format: Formatter;
 
 	constructor() {
 		this.djs = DjsUtils;
 		this.js = JsUtils;
+		this.format = Formatter;
 	}
 }
