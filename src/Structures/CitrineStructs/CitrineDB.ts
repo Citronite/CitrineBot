8af1e50c54b3ad3bb7a368a71609f25929bb896e
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-22 02:37:30 +0800

    Added more TODOs to CitrineDB

--- src/Structures/CitrineStructs/CitrineDB.ts
+++ src/Structures/CitrineStructs/CitrineDB.ts
@@ -1,11 +1,43 @@
 import * as Keyv from 'keyv';
+import { GuildID, IGuildConfig } from 'typings';
+import { GuildConfig } from '../../Utils/GuildConfig';
+import { Guild } from 'discord.js';
 
 export class CitrineDB {
 	constructor() {
 		// TODO
 	}
 
-	public add(name: string) {
+	public create(name: string) {
 		return name;
+	}
+
+	public async getGuild(id: GuildID): Promise<GuildConfig | null> {
+		try {
+			const jsonData = await this.client.db.guilds.get(id);
+			const conf = new GuildConfig(jsonData);
+			return Promise.resolve(conf);
+		} catch (err) {
+			return Promise.resolve(null);
+		}
+	}
+
+	public async setGuild(guild: Guild | IGuildConfig): Promise<GuildConfig | null> {
+		try {
+			const conf = new GuildConfig(guild);
+			await this.client.db.guilds.set(guild.id, conf.toJSON);
+			return Promise.resolve(conf);
+		} catch (err) {
+			return Promise.resolve(null);
+		}
+	}
+
+	public async unsetGuild(id: GuildID): Promise<boolean> {
+		try {
+			await this.client.db.guilds.delete(id);
+			return Promise.resolve(true);
+		} catch (err) {
+			return Promise.resolve(false);
+		}
 	}
 }
