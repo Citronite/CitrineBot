d8f0394ecb39714452e07e337a13c3348ef444b4
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-22 21:23:08 +0800

    [CitrineStructs/CitrineDB] Temp fixes to keep TS quiet. Will fix later...

--- src/Structures/CitrineStructs/CitrineDB.ts
+++ src/Structures/CitrineStructs/CitrineDB.ts
@@ -2,10 +2,17 @@ import * as Keyv from 'keyv';
 import { GuildID, IGuildConfig } from 'typings';
 import { GuildConfig } from '../../Utils/GuildConfig';
 import { Guild } from 'discord.js';
+import { CitrineClient } from '../CitrineClient';
 
 export class CitrineDB {
-	constructor() {
+	public readonly guilds: Map<string, IGuildConfig>;
+	public readonly client: CitrineClient;
+
+	constructor(client: CitrineClient) {
 		// TODO
+		// THIS IS SUPPOSED TO BE A KEYV DB BTW!
+		this.guilds = new Map();
+		this.client = client;
 	}
 
 	public create(name: string) {
@@ -14,10 +21,14 @@ export class CitrineDB {
 
 	public async getGuild(id: GuildID): Promise<GuildConfig | null> {
 		try {
-			const jsonData = await this.client.db.guilds.get(id);
-			const conf = new GuildConfig(jsonData);
-			return Promise.resolve(conf);
+			const jsonData = this.guilds.get(id);
+			if (jsonData) {
+				const conf = new GuildConfig(jsonData);
+				return Promise.resolve(conf);
+			}
+			return Promise.resolve(null);
 		} catch (err) {
+			this.client.logger.warn();
 			return Promise.resolve(null);
 		}
 	}
@@ -25,18 +36,21 @@ export class CitrineDB {
 	public async setGuild(guild: Guild | IGuildConfig): Promise<GuildConfig | null> {
 		try {
 			const conf = new GuildConfig(guild);
-			await this.client.db.guilds.set(guild.id, conf.toJSON);
+			this.guilds.set(guild.id, conf);
+
 			return Promise.resolve(conf);
 		} catch (err) {
+			this.client.logger.warn();
 			return Promise.resolve(null);
 		}
 	}
 
 	public async unsetGuild(id: GuildID): Promise<boolean> {
 		try {
-			await this.client.db.guilds.delete(id);
+			this.guilds.delete(id);
 			return Promise.resolve(true);
 		} catch (err) {
+			this.client.logger.warn();
 			return Promise.resolve(false);
 		}
 	}
