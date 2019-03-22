32302b6f5622a4881789fef7dd4602e05401027c
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-22 21:50:58 +0800

    [CitrineStructs/CitrineUtils] Update types

--- src/Structures/CitrineStructs/CitrineUtils.ts
+++ src/Structures/CitrineStructs/CitrineUtils.ts
@@ -1,11 +1,12 @@
 import { DjsUtils } from '../../Utils/DjsUtils';
 import { JsUtils } from '../../Utils/JsUtils';
 import { Formatter } from '../../Utils/Formatter';
+import { IDjsUtils, IJsUtils, IFormatter } from 'typings';
 
 export class CitrineUtils {
-	public readonly djs: DjsUtils;
-	public readonly js: JsUtils;
-	public readonly format: Formatter;
+	public readonly djs: IDjsUtils;
+	public readonly js: IJsUtils;
+	public readonly format: IFormatter;
 
 	constructor() {
 		this.djs = DjsUtils;
