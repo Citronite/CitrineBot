a0ee9c9dc31a50d6820cc0c89c930bc8d77cbec4
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-22 21:23:55 +0800

    [CitrineStructs/CitrineLogger] Added more TODOs...

--- src/Structures/CitrineStructs/CitrineLogger.ts
+++ src/Structures/CitrineStructs/CitrineLogger.ts
@@ -1,7 +1,28 @@
 import * as winston from 'winston';
+import { CitrineClient } from '../CitrineClient';
 
 export class CitrineLogger {
-	constructor() {
+	public readonly client: CitrineClient;
+
+	constructor(client: CitrineClient) {
 		// Implement this :P
+
+		this.client = client;
+	}
+
+	public error(): void {
+		return;
+	}
+
+	public info(): void {
+		return;
+	}
+
+	public warn(): void {
+		return;
+	}
+
+	public log(): void {
+		return;
 	}
 }
