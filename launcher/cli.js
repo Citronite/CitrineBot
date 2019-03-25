8dd2b6e43cbcba0680f124c47be87792b737129d
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-26 00:19:49 +0800

    [launcher/cli] Add comments

--- launcher/cli.js
+++ launcher/cli.js
@@ -1,4 +1,5 @@
 const readline = require('readline');
+const AbstractMenu = require('./Menus/AbstractMenu.js');
 
 const prompt = '-> ';
 const rl = readline.createInterface({
@@ -8,16 +9,20 @@ const rl = readline.createInterface({
 	// removeHistoryDuplicates: true
 });
 
+// Keep track of the current Menu being displayed in the console
 rl.currMenu = undefined;
 
+// Simple shorthand function for console.log
 function println(str) {
 	console.log(str + '\n');
 }
 
+// Another rather redundant shorthand :P
 function print(...args) {
 	console.log(...args);
 }
 
+// Useful sleep function for better UX
 async function sleep(time = 1000) {
 	return new Promise((res) => {
 		setTimeout(() => {
@@ -26,6 +31,7 @@ async function sleep(time = 1000) {
 	});
 }
 
+// Self-explanatory
 function printHeader() {
 	console.log('\n' +
 	'\t#############################\n' +
@@ -37,6 +43,7 @@ function printHeader() {
 	'\t#############################\n');
 }
 
+// Obtain user input.
 function input(str) {
 	return new Promise((res) => {
 		rl.question(`\n${str}\n${prompt}`, (ans) => {
@@ -46,21 +53,30 @@ function input(str) {
 	});
 }
 
+// For yes/no questions
 async function confirm(str) {
-	while (true) {
+	// Basically an infinite loop, only stops after receiving proper input
+	while (str) {
 		const ans = await input(str);
 		if (['yes', 'y'].includes(ans.toLowerCase())) return true;
 		if (['no', 'n'].includes(ans.toLowerCase())) return false;
 	}
 }
 
+// Clear console screen. By default, it clears everything BELOW the header
+// Use cls(0, 0) to clear the whole screen
 function cls(x = 0, y = 8) {
 	readline.cursorTo(process.stdout, x, y);
 	readline.clearScreenDown(process.stdout);
 }
 
+// Takes a Menu (must be instance of an extension of the AbstractMenu class)
+// and prints it to console.
+// Also sets rl.currMenu to that menu. This, along with <Menu>.code can be helpful
+// For identifying which menu is currently displayed to the user.
 async function printMenu(menu) {
 	if (!menu) return;
+	if (!(menu instanceof AbstractMenu)) return;
 
 	println(menu.title);
 
@@ -83,6 +99,7 @@ async function printMenu(menu) {
 	rl.prompt();
 }
 
+// Exports
 module.exports = {
 	print,
 	println,
