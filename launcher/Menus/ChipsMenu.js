17bc8670c3e59656191c1a26a5a7c842bdf47ef5
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-26 00:48:04 +0800

    [Menus/ChipsMenu] Create a new launcher menu for managing chips

---
+++ launcher/Menus/ChipsMenu.js
@@ -0,0 +1,43 @@
+const AbstractMenu = require('./AbstractMenu.js');
+const fs = require('fs');
+const { println } = require('../cli.js');
+
+class ChipsMenu extends AbstractMenu {
+	constructor() {
+		super({
+			title: 'What would you like to do?',
+			choices: [
+				'List installed Chips',
+				'Download Chip from GitHub',
+				'Delete Chip',
+				'Create new Chip',
+			],
+		});
+
+		this.code = 1;
+	}
+
+	1() {
+		const files = fs.readdirSync('./bin/Chips/');
+		for (const file of files) {
+			println(file);
+		}
+	}
+
+	2() {
+		println('Download Chip from GitHub');
+		println('To be implemented!');
+	}
+
+	3() {
+		println('Delete Chip');
+		println('To be implemented!');
+	}
+
+	4() {
+		println('Create new Chip');
+		println('To be implemented!');
+	}
+}
+
+module.exports = ChipsMenu;
