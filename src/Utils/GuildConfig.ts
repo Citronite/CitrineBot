bc66de4e21dfdb1e87cce371a2e7c6a24c5870e5
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-30 15:28:56 +0800

    [Utils/GuildConfig] Make guildconfig data private, and add all the getters/setters like in CitrineSettings

--- src/Utils/GuildConfig.ts
+++ src/Utils/GuildConfig.ts
@@ -1,6 +1,5 @@
 import { Guild } from 'discord.js';
 import {
-	IGuildConfig,
 	GuildID,
 	ChannelID,
 	RoleID,
@@ -20,34 +19,28 @@ const props = [
 	'reqRoles'
 ];
 
-export class GuildConfig implements IGuildConfig {
+export class GuildConfig {
 	public readonly id: GuildID;
-	public prefix: string;
-	public disabledRole: RoleID;
-	public deleteCmdCalls: boolean;
-	public deleteCmdCallsDelay: number;
-	public readMsgEdits: boolean;
-	public disabledUsers: Set<UserID>;
-	public disabledChannels: Set<ChannelID>;
-	public disabledCommands: Set<string>;
-	public reqRoles: { [cmd in string]: RoleID | undefined };
+	private readonly data: any;
 
-	constructor(guild: Guild | IGuildConfig) {
+	constructor(guild: Guild | GuildConfig) {
 		if (guild instanceof Guild) {
 			// Have to use any here since TS doesn't know guild.client
 			// would be CitrineClient instead of Client.
 			const client: any = guild.client;
-
 			this.id = guild.id;
-			this.prefix = client.settings.global.prefix;
-			this.disabledRole = '';
-			this.deleteCmdCalls = false;
-			this.deleteCmdCallsDelay = 2000;
-			this.readMsgEdits = false;
-			this.disabledUsers = new Set();
-			this.disabledChannels = new Set();
-			this.disabledCommands = new Set();
-			this.reqRoles = {};
+			this.data = {
+				id: guild.id,
+				prefix: client.settings.globalPrefix,
+				disabledRole: '',
+				deleteCmdCalls: false,
+				deleteCmdCallsDelay: 2000,
+				readMsgEdits: false,
+				disabledUsers: new Set(),
+				disabledChannels: new Set(),
+				disabledCommands: new Set(),
+				reqRoles: {},
+			};
 
 		}	else {
 
@@ -56,33 +49,121 @@ export class GuildConfig implements IGuildConfig {
 			});
 
 			this.id = guild.id;
-			this.prefix = guild.prefix;
-			this.disabledRole = guild.disabledRole;
-			this.deleteCmdCalls = guild.deleteCmdCalls;
-			this.deleteCmdCallsDelay = guild.deleteCmdCallsDelay;
-			this.readMsgEdits = guild.readMsgEdits;
-			this.disabledUsers = new Set(guild.disabledUsers);
-			this.disabledChannels = new Set(guild.disabledChannels);
-			this.disabledCommands = new Set(guild.disabledCommands);
-			this.reqRoles = guild.reqRoles;
+			this.data = {
+				id: guild.id,
+				prefix: guild.prefix,
+				disabledRole: guild.disabledRole,
+				deleteCmdCalls: guild.deleteCmdCalls,
+				deleteCmdCallsDelay: guild.deleteCmdCallsDelay,
+				readMsgEdits: guild.readMsgEdits,
+				disabledUsers: new Set(guild.disabledUsers),
+				disabledChannels: new Set(guild.disabledChannels),
+				disabledCommands: new Set(guild.disabledCommands),
+				reqRoles: guild.reqRoles,
+			};
 
 		}
 	}
 
+	get prefix(): string {
+		return this.data.prefix;
+	}
+
+	set prefix(str: string) {
+		this.data.prefix = str;
+	}
+
+	get disabledRole(): string {
+		return this.data.disabledRole;
+	}
+
+	set disabledRole(id: string) {
+		this.data.disabledRole = id;
+	}
+
+	get deleteCmdCalls(): boolean {
+		return this.data.deleteCmdCalls;
+	}
+
+	set deleteCmdCalls(val: boolean) {
+		this.data.deleteCmdCalls = val;
+	}
+
+	get deleteCmdCallsDelay(): number {
+		return this.data.deleteCmdCallsDelay;
+	}
+
+	set deleteCmdCallsDelay(val: number) {
+		if (val >= 3600000 && val <= 1000) {
+			this.data.deleteCmdCallsDelay = val;
+		} else {
+			this.data.deleteCmdCallsDelay = 5000;
+		}
+	}
+
+	get readMsgEdits(): boolean {
+		return this.data.readMsgEdits;
+	}
+
+	set readMsgEdits(val: boolean) {
+		this.data.readMsgEdits = val;
+	}
+
+	get disabledUsers(): UserID[] {
+		return [...this.data.disabledUsers];
+	}
+
+	public disableUser(id: UserID): void {
+		this.data.disabledUsers.add(id);
+	}
+
+	public enableUser(id: UserID): void {
+		this.data.disabledUsers.delete(id);
+	}
+
+	get disabledChannels(): ChannelID[] {
+		return [...this.data.disabledChannels];
+	}
+
+	public disableChannel(id: ChannelID): void {
+		this.data.disabledChannels.add(id);
+	}
+
+	public enableChannel(id: ChannelID): void {
+		this.data.disabledChannels.delete(id);
+	}
+
+	get disabledCommands(): string[] {
+		return [...this.data.disabledCommands];
+	}
+
+	public disableCommand(id: string): void {
+		this.data.disabledCommands.add(id);
+	}
+
+	public enableCommand(id: string): void {
+		this.data.disabledCommands.delete(id);
+	}
+
+	get reqRoles(): object {
+		return this.data.reqRoles;
+	}
+
 	public setReqRole(cmd: string, role: RoleID): void {
-		this.reqRoles[cmd] = role;
+		this.data.reqRoles[cmd] = role;
 	}
 
 	public unsetReqRole(cmd: string): void {
-		this.reqRoles[cmd] = undefined;
+		this.data.reqRoles[cmd] = undefined;
 	}
 
 	public toJSON(): object {
+		const conf = this.data;
 		return {
-			...this,
-			disabledUsers: [...this.disabledUsers],
-			disabledChannels: [...this.disabledChannels],
-			disabledCommands: [...this.disabledCommands],
+			...conf,
+			disabledUsers: [...conf.disabledUsers],
+			disabledChannels: [...conf.disabledChannels],
+			disabledCommands: [...conf.disabledCommands],
 		};
 	}
 
