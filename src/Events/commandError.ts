e5006011f8a050e8687bca5bcf7d6c34b72f4a4e
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-31 20:12:15 +0800

    [Events/commandError] Formatting

--- src/Events/commandError.ts
+++ src/Events/commandError.ts
@@ -3,13 +3,13 @@ import { CommandError } from '../Structures/ErrorStructs/CommandError';
 import { QuickEmbed } from '../Utils/QuickEmbed';
 
 module.exports = {
-	name: 'commandError',
-	maxListeners: '1',
-	listener: (ctx: Context, error: CommandError): void => {
+  name: 'commandError',
+  maxListeners: '1',
+  listener: (ctx: Context, error: CommandError): void => {
     if ([200, 201, 202].includes(error.code)) {
       ctx.send(QuickEmbed.commandHelp(ctx, error.cmd));
       return;
     }
     ctx.send(error.toEmbed());
-	}
+  }
 };
