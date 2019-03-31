fd950880e540436e08a6fdd2c52a7a975c101220
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-31 19:46:29 +0800

    launcher.bat renamed to start_launcher.bat

--- launcher.bat
+++
@@ -1,1 +0,0 @@
-node .\launcher\launcher.js
---
+++ start_launcher.bat
@@ -0,0 +1,4 @@
+:: At the moment, this file will simply start the launcher.
+:: But eventually, it will also check if the correct version
+:: of nodejs is installed.
+node launcher
