08cb2f282c0b8397b8c56b40f0d65d9fbf1db72f
Quantomistro3178 <m.saifi3178@gmail.com>
2019-03-31 20:03:47 +0800

    I think I broke something 😳

--- src/Events/commandError.ts
+++ src/Events/commandError.ts
@@ -1,10 +1,15 @@
 import { Context } from '../Utils/Context';
 import { CommandError } from '../Structures/ErrorStructs/CommandError';
+import { QuickEmbed } from '../Utils/QuickEmbed';
 
 module.exports = {
 	name: 'commandError',
 	maxListeners: '1',
 	listener: (ctx: Context, error: CommandError): void => {
-		ctx.send(error.toEmbed());
+    if ([200, 201, 202].includes(error.code)) {
+      ctx.send(QuickEmbed.commandHelp(ctx, error.cmd));
+      return;
+    }
+    ctx.send(error.toEmbed());
 	}
 };
---
+++ start_citrine.bat
@@ -0,0 +1,2 @@
+:: Simply run the node script to start the bot
+node run
