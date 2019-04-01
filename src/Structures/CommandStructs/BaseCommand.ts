ccc273355d02a703c8d0064b1e1a170586272cbe
Quantomistro3178 <m.saifi3178@gmail.com>
2019-04-01 16:26:21 +0800

    [CommandStructs/BaseCommand] Added a comment

--- src/Structures/CommandStructs/BaseCommand.ts
+++ src/Structures/CommandStructs/BaseCommand.ts
@@ -1,12 +1,16 @@
 import { AbstractCommand } from './AbstractCommand';
 import { CommandOptions } from 'typings';
+import { sep } from 'path';
+
+// Use this for chipName:
+// const chipName = () => __dirname.split(sep).slice(-1)[0];
 
 export class BaseCommand extends AbstractCommand {
   public readonly chip: string;
 
-  constructor(name: string, options: CommandOptions) {
+  constructor(name: string, chip: string, options: CommandOptions) {
     super(name, options);
 
-    this.chip = __filename;
+    this.chip = chip || '--/--';
   }
 }
