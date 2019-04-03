edae521eb4ffe5801c258461376d88843750bb5c
Quantomistro3178 <m.saifi3178@gmail.com>
2019-04-03 14:06:30 +0800

    Add more stuff to exports

--- src/exports.ts
+++ src/exports.ts
@@ -3,11 +3,18 @@ import { SubCommand } from './Structures/CommandStructs/SubCommand';
 import { BaseError } from './Structures/ErrorStructs/BaseError';
 import { CommandError } from './Structures/ErrorStructs/CommandError';
 import { ErrorCodes } from './Structures/ErrorStructs/ErrorCodes';
+import { QuickEmbed } from './Utils/QuickEmbed';
+import { Colours, Colors } from './Utils/Constants';
+import * as promisefs from './Utils/promisefs';
 
 module.exports = {
   BaseCommand,
   SubCommand,
   BaseError,
   CommandError,
-  ErrorCodes
+  ErrorCodes,
+  QuickEmbed,
+  Colours,
+  Colors,
+  promisefs
 };
