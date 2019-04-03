886a1bacd2dc8b239378d32859106682c2e04ee8
Quantomistro3178 <m.saifi3178@gmail.com>
2019-04-03 15:51:00 +0800

    [Utils/Context] Added useful perm check functions

--- src/Utils/Context.ts
+++ src/Utils/Context.ts
@@ -1,8 +1,8 @@
-import { IContext } from 'typings';
 import { QuickEmbed } from './QuickEmbed';
 import { CitrineClient } from '../Structures/CitrineClient';
+import { BaseError } from '../Structures/ErrorStructs/BaseError';
+import { ErrorCodes } from '../Structures/ErrorStructs/ErrorCodes';
 import {
-  Client,
   Message,
   User,
   TextChannel,
@@ -10,24 +10,33 @@ import {
   GroupDMChannel,
   MessageReaction,
   Emoji,
-  ReactionEmoji
+  ReactionEmoji,
+  PermissionResolvable,
+  GuildMember,
+  Guild
 } from 'discord.js';
 
 type reaction = string | Emoji | ReactionEmoji;
 
-export class Context implements IContext {
-  public readonly client: any; // This is actually CitrineClient but TypeScript is dumb, so yeah.
-  public readonly invokedPrefix: string;
+export class Context {
+  public readonly client: CitrineClient | any;
+  public readonly prefix: string;
   public readonly message: Message;
   public readonly author: User;
+  public readonly member: GuildMember | null;
   public readonly channel: TextChannel | DMChannel | GroupDMChannel;
+  public readonly guild: Guild | null;
 
   constructor(message: Message, iPrefix: string) {
     this.client = message.client;
-    this.invokedPrefix = iPrefix;
+    // The prefix used to invoke the command.
+    // If it was a bot mention, then this will be the global prefic for the bot.
+    this.prefix = iPrefix;
     this.message = message;
     this.author = message.author;
+    this.member = message.member || null;
     this.channel = message.channel;
+    this.guild = message.guild || null;
   }
 
   public async success(msg: string, embed: boolean = true): Promise<Message | Message[]> {
@@ -54,14 +63,16 @@ export class Context implements IContext {
   }
 
   public async prompt(msg: string, contentOnly: boolean = true, timeOut: number = 30000): Promise<Message | string | null> {
+    // contentOnly specifies whether this function will return only the content of the answer, or
+    // the entire Message object from the user.
     return Promise.reject('Not Implemented');
-    // Implement using a reaction collector? idk :v
   }
 
   public async promptReaction(msg: string, emojis: reaction[], limit: number = 1, timeOut: number = 30000): Promise<MessageReaction[] | null> {
     return Promise.reject('Not Implemented');
-    // Kinda like awaitReply, but waits for a reaction on the bot message. Could be used for yes/no stuff?
+    // Waits for a reaction on the bot message.
     // First param would be msg to send, second param list of reactions to await.
+    // Limit is number of reactions to wait for.
   }
 
   public async send(...args: any): Promise<Message | Message[]> {
@@ -74,5 +85,32 @@ export class Context implements IContext {
 
   public async sendDM(...args: any): Promise<Message | Message[]> {
     return this.author.send(...args);
+  }
+
+  public checkPerms(perms: PermissionResolvable, checkBot: boolean = true, checkAdmin: boolean = true): void {
+    const { checkDiscordPerms } = this.client.permHandler;
+
+    if (!this.member) {
+      throw new BaseError(ErrorCodes.PERMISSION_ERROR, ['Permission checks will only work on guild members, not users!']);
+    }
+
+    checkDiscordPerms(this.channel, this.member, perms, checkAdmin);
+
+    if (checkBot) {
+      if (!this.guild) {
+        throw new BaseError(ErrorCodes.PERMISSION_ERROR, ['Permission checks will only work inside guilds!']);
+      }
+      checkDiscordPerms(this.channel, this.guild.me, perms, checkAdmin);
+    }
+  }
+
+  public checkBotDev(): void {
+    if (this.client.settings.devs.includes(this.author.id)) return;
+    throw new BaseError(ErrorCodes.PERMISSION_ERROR, ['Only bot developers may perform this action!']);
+  }
+
+  public checkBotOwner(): void {
+    if (this.client.settings.owner === this.author.id) return;
+    throw new BaseError(ErrorCodes.PERMISSION_ERROR, ['Only bot owners may perform this action!']);
   }
 }
