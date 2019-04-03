feb1fa2255656bfc0907eba66655f99cbf12f1da
Quantomistro3178 <m.saifi3178@gmail.com>
2019-04-03 17:05:58 +0800

    [Structures/CitrineClient] Implement initChips and initEvents. Finally.

--- src/Structures/CitrineClient.ts
+++ src/Structures/CitrineClient.ts
@@ -6,16 +6,25 @@ import { Client, ClientOptions } from 'discord.js';
 import { CmdHandler } from './Handlers/CmdHandler';
 import { PermHandler } from './Handlers/PermHandler';
 import { ICmdHandler, IPermHandler } from 'typings';
+import { BaseCommand } from './CommandStructs/BaseCommand';
+import { Collection } from 'discord.js';
 import * as fs from 'fs';
+import * as pfs from '../Utils/promisefs';
+
+function fileFilter(arr: string[]): string[] {
+  return arr.filter(val => {
+    return !val.startsWith('_') && val.endsWith('.js');
+  });
+}
 
 export class CitrineClient extends Client {
   public readonly settings: CitrineSettings;
   public readonly logger: CitrineLogger;
   public readonly utils: CitrineUtils;
   public readonly db: CitrineDB;
-
   public readonly cmdHandler: ICmdHandler;
   public readonly permHandler: IPermHandler;
+  public readonly commands: Collection<string, BaseCommand>;
 
   constructor(options: ClientOptions) {
     super(options);
@@ -27,18 +36,54 @@ export class CitrineClient extends Client {
 
     this.cmdHandler = CmdHandler;
     this.permHandler = PermHandler;
+
+    this.commands = new Collection();
   }
 
   public initChips(defaultChips: string[]): boolean {
-    return !!defaultChips;
+    try {
+      const defaults = ['core'].concat(defaultChips);
+
+      for (const chip of defaults) {
+        const files = fs.readdirSync(`./bin/Chips/${chip}`);
+        const cmdFiles = fileFilter(files);
+        for (const file of cmdFiles) {
+          const cmd = require(`../Chips/${chip}/${file}`);
+          this.commands.set(cmd.name, cmd);
+        }
+        if (files.includes('_onload.js')) {
+          const onload = require(`../Chips/${chip}/_onload.js`);
+          if (typeof onload === 'function') onload(null);
+        }
+        this.logger.info(`Loaded Chip [${chip}]`);
+      }
+
+      this.logger.info('Successfully initialized default Chips!');
+      return true;
+    } catch (err) {
+      this.logger.error(`Error initializing default Chips:\n  ${err.stack}`);
+      return false;
+    }
   }
 
   public initEvents(): boolean {
-    return 1 > 0;
-  }
+    try {
+      const eventFiles = fileFilter(fs.readdirSync('./bin/Events'));
 
-  public initDB(): boolean {
-    return 1 > 0;
+      for (const file of eventFiles) {
+        const event = require(`../Events/${file}`);
+        if (event.listener) this.on(event.name, event.listener.bind(null, this));
+        if (event.once) this.once(event.name, event.once.bind(null, this));
+        // Clear cache
+        delete require.cache[require.resolve(`../Events/${file}`)];
+      }
+
+      this.logger.info('Successfully initialized event listeners!');
+      return true;
+    } catch (err) {
+      this.logger.error(`Error initializing event listeners:\n  ${err.stack}`);
+      return false;
+    }
   }
 
   public async loadChip(): Promise<boolean> {
@@ -53,20 +98,16 @@ export class CitrineClient extends Client {
     return Promise.reject(false);
   }
 
-  public async reloadChip(): Promise<boolean> {
-    return Promise.reject(false);
-  }
-
   public async launch(): Promise<void> {
     // Starts bot!
     try {
-      console.log('Fetching data. . .');
+      this.logger.info('Fetching data. . .');
       const { TOKEN, prefix } = require('../../data/core/_settings.json');
       await this.settings.load();
 
       if (this.settings.globalPrefix === 'DEFAULT') this.settings.globalPrefix = prefix;
 
-      console.log('Logging in to Discord. . .');
+      this.logger.info('Logging in to Discord. . .');
       await this.login(TOKEN);
 
       if (this.settings.owner === 'DEFAULT') {
