c9261e709898bbfc8169d8c54b6455e9308f66d0
Quantomistro3178 <m.saifi3178@gmail.com>
2019-04-03 19:24:54 +0800

    Bug fix

--- src/Structures/CommandStructs/BaseCommand.ts
+++ src/Structures/CommandStructs/BaseCommand.ts
@@ -1,6 +1,6 @@
 import { AbstractCommand } from './AbstractCommand';
 import { CommandOptions } from 'typings';
-import { sep } from 'path';
+// import { sep } from 'path';
 
 // Use this for chipName:
 // const chipName = () => __dirname.split(sep).slice(-1)[0];
@@ -9,8 +9,11 @@ export class BaseCommand extends AbstractCommand {
   public readonly chip: string;
 
   constructor(name: string, chip: string, options: CommandOptions) {
-    super(name, options);
+    if (typeof name !== 'string') throw Error('Command name must be a string!');
+    if (typeof chip !== 'string') throw Error('Command chip must be a string!');
+    if (typeof options !== 'object') throw Error('Command options must be an object!');
 
-    this.chip = chip || '--/--';
+    super(name, options);
+    this.chip = chip;
   }
 }
