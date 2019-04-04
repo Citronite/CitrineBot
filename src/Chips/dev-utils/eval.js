1a2a2b230f1205dd874c4f14ed168d6270cf1e81
Quantomistro3178 <m.saifi3178@gmail.com>
2019-04-04 14:16:51 +0800

    [dev-utils/eval] Add permission checks

--- src/Chips/dev-utils/eval.js
+++ src/Chips/dev-utils/eval.js
@@ -3,19 +3,26 @@ const { BaseCommand } = require('../../exports');
 class Eval extends BaseCommand {
   constructor() {
     super('eval', 'dev-utils', {
-      description: 'Execute arbitrary JavaScript code.',
+      description: 'Evaluate arbitrary JavaScript code.',
       usage: '[p]eval <code>'
     });
   }
 
   execute(ctx, ...args) {
-    ctx.checkDM();
-    try { ctx.checkBotOwner(); }
-    catch (_) { ctx.checkBotDev(); }
+    // Permission checks!
+    try {
+      // First check if the author is the bot owner
+      ctx.checkBotOwner();
+    }
+    catch (_) {
+      // If not, check if the author is a bot developer
+      ctx.checkBotDev();
+      // Finally, lock the command to only dm channels!
+      ctx.lock(ctx.channel.type === 'dm');
+    }
 
     const code = args.join(' ');
     ctx.send(`[Code]\n${code}`, { code: 'js', split: true });
-
 		try {
 			const result = eval(code);
       ctx.send(`[Result]\n${result}`, { code: 'js', split: true });
