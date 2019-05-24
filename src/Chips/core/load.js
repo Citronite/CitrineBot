c5a626ba294f961d66b28e12675a3bc5aad914cf
Quantomistro3178 <m.saifi3178@gmail.com>
2019-05-24 23:09:13 +0800

    [Chips/core] Add load command

---
+++ src/Chips/core/load.js
@@ -0,0 +1,33 @@
+const { BaseCommand, QuickEmbed } = require('../../exports');
+const { promisify } = require('util');
+const { readdir } = require('fs');
+
+const readdirAsync = promisify(readdir);
+
+class Load extends BaseCommand {
+  constructor() {
+    super({
+      name: 'load',
+      description: 'Loads a chip. Separate multiple names with spaces, or enter `all` to load all. Optionally, add the `-re` flag to reload/update chips',
+      usage: '[p]load <...chips | "all"> [-re]'
+    }, 'core');
+  }
+
+  async execute(ctx, ...chips) {
+    ctx.lock('botOwner');
+
+    const allChips = await readdirAsync('./bin/Chips');
+    let filteredChips = chips.includes('all') ? allChips : chips.filter(name => allChip.includes(name));
+    for (const chip of filteredChips) {
+        if (chips.includes('-re')) {
+            await ctx.client.clearCachedChip(chip);
+        }
+        await ctx.client.loadChip(chip);
+    }
+    const embed = QuickEmbed.success(`Successfully loaded chip(s):\n${filteredChips.join('\n')}`);
+    await ctx.send(embed);
+    return;
+    }
+}
+
+module.exports = new Load();
