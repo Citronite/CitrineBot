27c7ef647ea465a4d5d46096bd5fb2db76aa27fd
Quantomistro3178 <m.saifi3178@gmail.com>
2019-05-25 11:08:53 +0800

    [Chips/core] Add prefix subcommand

---
+++ src/Chips/core/_gconfig/prefix.js
@@ -0,0 +1,25 @@
+const { SubCommand } = require('../../../exports');
+
+class Prefix extends SubCommand {
+    constructor() {
+      super({
+        name: 'prefix',
+        description: 'Change Citrine\'s global prefix.',
+        usage: '[p]gconfig prefix <new prefix>'
+      });
+    }
+  
+    async execute(ctx, prefix) {
+        if (prefix) {
+            ctx.client.settings.globalPrefix = prefix;
+            await ctx.client.settings.save();
+            await ctx.success(`Successfully changed the global prefix to ${prefix}`);
+            return;
+        }
+        else {
+            await ctx.send(`The current global prefix is \`${ctx.client.settings.globalPrefix}\``);
+            return;
+        }
+    }
+}
+module.exports = new Prefix();
