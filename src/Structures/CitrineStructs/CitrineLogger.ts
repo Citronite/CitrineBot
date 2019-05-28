62cad2a21d39a40269bbdbdc48bccc25c0393bfe
Quantomistro3178 <m.saifi3178@gmail.com>
2019-05-28 16:04:50 +0800

    Remove unnecessary code from logger

--- src/Structures/CitrineStructs/CitrineLogger.ts
+++ src/Structures/CitrineStructs/CitrineLogger.ts
@@ -1,22 +1,15 @@
 import * as winston from 'winston';
-import { CitrineClient } from '../CitrineClient';
 
 export class CitrineLogger {
-  public readonly client: CitrineClient;
-
-  constructor(client: CitrineClient) {
-    this.client = client;
-  }
-
-  public error(err: string | Error): void {
-    console.log(err);
-  }
-
   public info(str: string): void {
     console.log(str);
   }
 
   public warn(str: string): void {
     console.log(str);
+  }
+
+  public error(err: string | Error): void {
+    console.log(err);
   }
 }
