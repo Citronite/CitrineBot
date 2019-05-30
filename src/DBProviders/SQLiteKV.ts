832a908ffeecb2be9b10ee474af92f59b0bccc21
Quantomistro3178 <m.saifi3178@gmail.com>
2019-05-30 11:57:28 +0800

    Use KeyvWrapper in SQLiteKV

--- src/DBProviders/SQLiteKV.ts
+++ src/DBProviders/SQLiteKV.ts
@@ -1,13 +1,14 @@
 import Keyv = require('keyv');
 import { resolve } from 'path';
 import { DbProvider, DbConnection } from 'typings';
+import { KeyvWrapper } from './Utils/KeyvWrapper';
 
 export class SQLiteKV implements DbProvider {
   [key: string]: any;
 
   public connect(name: string, path: string): DbConnection {
     try {
-      this[name] = new Keyv(`sqlite://${resolve(path)}`);
+      this[name] = new KeyvWrapper(`sqlite://${resolve(path)}`);
       return this[name];
     } catch (err) {
       throw err;
