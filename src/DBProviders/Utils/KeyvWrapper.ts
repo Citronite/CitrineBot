b126d2226ffd5db3ccc82fdcfc48c891261b2c45
Quantomistro3178 <m.saifi3178@gmail.com>
2019-05-30 11:56:35 +0800

    Add KeyvWrapper for CRUD methods

---
+++ src/DBProviders/Utils/KeyvWrapper.ts
@@ -0,0 +1,24 @@
+import Keyv = require('keyv');
+import { DbConnection } from 'typings';
+
+export class KeyvWrapper extends Keyv implements DbConnection {
+  constructor(...options: any[]) {
+      super(...options);
+    }
+
+  public async create(key: string, value: any, ttl?: number): Promise<true> {
+    return this.set(key, value, ttl);
+  }
+
+  public async read(key: string): Promise<any> {
+    return this.get(key);
+  }
+
+  public async update(key: string, value: any, ttl?: number): Promise<true> {
+    return this.set(key, value, ttl);
+  }
+
+  public async delete(key: string): Promise<true> {
+    return this.delete(key);
+  }
+}
