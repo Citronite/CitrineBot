fc95b25e379b69d55426464bb066ae8a142a3362
Quantomistro3178 <m.saifi3178@gmail.com>
2019-06-04 13:51:54 +0800

    [CommandStore] Experimental

---
+++ src/Structures/Command/CommandStore.ts
@@ -0,0 +1,15 @@
+/*
+import { Collection } from 'discord.js';
+import { BaseCommand } from './BaseCommand';
+import { CitrineClient } from '../CitrineClient';
+
+export class CommandStore {
+    private readonly client: CitrineClient;
+    public readonly commands: Collection<string, BaseCommand>;
+
+    constructor(client: CitrineClient) {
+        this.client = client;
+        this.commands = new Collection();
+    }
+}
+*/
