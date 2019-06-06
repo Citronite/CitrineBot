0282d20f4de3628929cf5f788e36f9f279df9dc8
Quantomistro3178 <m.saifi3178@gmail.com>
2019-06-06 23:00:30 +0800

    PermHandler#checkFilters now takes context in param

--- src/Structures/Handlers/PermHandler.ts
+++ src/Structures/Handlers/PermHandler.ts
@@ -1,9 +1,8 @@
 import { GuildConfig } from '../../Utils/GuildConfig';
 import { Exception } from '../Exceptions/Exception';
 import { ExceptionCodes } from '../Exceptions/ExceptionCodes';
-import { Command } from '../Command/AbstractCommand';
+import { Context } from '../../Utils/Context';
 import {
-  Message,
   GuildMember,
   TextChannel,
   PermissionResolvable,
@@ -12,31 +11,29 @@ import {
   User
 } from 'discord.js';
 
-export class PermHandler {
-  public async checkFilters(cmd: Command, message: Message | any): Promise<void> {
-    const { settings: globalConfig, db } = message.client;
-    try {
-      if (message.author.id === globalConfig.owner) return;
-      const errors = [];
-      const config: GuildConfig | null = await db.guilds.read(message.guild.id);
-      if (config) {
-        if (config.disabledUsers.includes(message.author.id)) errors.push('Disabled User [Local]');
-        if (config.disabledChannels.includes(message.channel.id)) errors.push('Disabled Channel');
-        if (config.disabledCommands.includes(cmd.name)) errors.push('Disabled Command [Local]');
-      }
-      if (globalConfig.disabledUsers.includes(message.author.id)) errors.push('Disabled User [Global]');
-      if (globalConfig.disabledCommands.includes(cmd.name)) errors.push('Disabled Command [Global]');
+const ErrCode = ExceptionCodes;
 
-      const code = ExceptionCodes.FAILED_CUSTOM_FILTERS;
-      if (errors.length) return Promise.reject(new Exception(code, errors));
-    } catch (err) {
-      return Promise.reject(err);
+export class PermHandler {
+  public checkFilters(ctx: Context, config?: GuildConfig): void {
+    const { settings: globalConfig } = ctx.client;
+    if (ctx.author.id === globalConfig.owner) return;
+
+    const errors = [];
+    if (config) {
+      if (config.disabledUsers.includes(ctx.author.id)) errors.push('Disabled User [Local]');
+      if (config.disabledChannels.includes(ctx.channel.id)) errors.push('Disabled Channel');
+      if (config.disabledCommands.includes(ctx.command.name)) errors.push('Disabled Command [Local]');
     }
+    if (globalConfig.disabledUsers.includes(ctx.author.id)) errors.push('Disabled User [Global]');
+    if (globalConfig.disabledCommands.includes(ctx.command.name)) errors.push('Disabled Command [Global]');
+
+    const code = ErrCode.FAILED_CUSTOM_FILTERS;
+    if (errors.length) throw new Exception(code, errors);
   }
 
-  public async checkDiscordPerms(perms: PermissionResolvable, member: GuildMember, channel: TextChannel, checkAdmin: boolean = true): Promise<void> {
+  public checkDiscordPerms(perms: PermissionResolvable, member: GuildMember, channel: TextChannel, checkAdmin: boolean = true): void {
     const memberPerms = channel.memberPermissions(member);
-    if (!memberPerms) throw new Exception(ExceptionCodes.NOT_FOUND, `Member permissions not found (id: ${member.id})`);
+    if (!memberPerms) throw new Exception(ErrCode.NOT_FOUND, `Member permissions not found (id: ${member.id})`);
 
     const missing = memberPerms.missing(perms, checkAdmin);
     if (!missing) return;
@@ -49,16 +46,16 @@ export class PermHandler {
 
   public checkGuildOwner(guild: Guild, user: User | GuildMember): void {
     if (guild.ownerID === user.id) return;
-    throw new Exception(ExceptionCodes.PERMISSION_ERROR, 'Only guild owners may perform that action!');
+    throw new Exception(ErrCode.PERMISSION_ERROR, 'Only guild owners may perform that action!');
   }
 
   public checkBotOwner(user: User | GuildMember | any): void {
     if (user.id === user.client.settings.owner) return;
-    throw new Exception(ExceptionCodes.PERMISSION_ERROR, 'Only bot owners may perform that action!');
+    throw new Exception(ErrCode.PERMISSION_ERROR, 'Only bot owners may perform that action!');
   }
 
   public checkBotDev(user: User | GuildMember | any): void {
     if (user.client.settings.devs.includes(user.id)) return;
-    throw new Exception(ExceptionCodes.PERMISSION_ERROR, 'Only bot developers may perform that action!');
+    throw new Exception(ErrCode.PERMISSION_ERROR, 'Only bot developers may perform that action!');
   }
 }
