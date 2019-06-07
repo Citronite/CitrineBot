432d4aaa11ee411b9138b00fa9ed8ce22848b8ea
Quantomistro3178 <m.saifi3178@gmail.com>
2019-06-07 15:00:07 +0800

    Lol

--- src/Chips/core/ping.js
+++ src/Chips/core/ping.js
@@ -11,7 +11,7 @@ class Ping extends BaseCommand {
   }
 
   async execute(ctx) {
-    await ctx.send(`🏓 **Pong!**\n${Math.floor(ctx.client.ping)}ms`);
+    await ctx.send(`🏓 **Pong!** ${Math.floor(ctx.client.ping)}ms`);
   }
 }
 
