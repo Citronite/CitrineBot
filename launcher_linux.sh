b182495fc00a2ab6e742ae38d100518eaeebe954
Quantomistro3178 <m.saifi3178@gmail.com>
2019-06-08 15:57:09 +0800

    Supply arguments to launcher_linux

--- launcher_linux.sh
+++ launcher_linux.sh
@@ -3,5 +3,5 @@
 
 clear
 echo -ne "\033]0;Citrine Launcher\007"
-node "$(dirname $BASH_SOURCE)/launcher/main.js"
+node "$(dirname $BASH_SOURCE)/launcher/main.js" $*
 read -n1 -r -p "Press any key to continue..."
