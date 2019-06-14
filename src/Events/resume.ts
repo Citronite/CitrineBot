f4ccead38d1361feff9d362f81605c7d3cca6ec6
Quantomistro3178 <m.saifi3178@gmail.com>
2019-06-14 21:26:25 +0800

    Move connection-related events to citrine's constructor

--- src/Events/resume.ts
+++
@@ -1,8 +0,0 @@
-import { CitrineClient } from '../Structures/CitrineClient';
-
-module.exports = {
-  name: 'resume',
-  listener: (client: CitrineClient) => {
-    client.logger.error('Connection resumed!');
-  }
-};
