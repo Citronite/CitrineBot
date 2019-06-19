748570ffcfcdc477e9f04b32d048f94e4ae12cce
Quantomistro3178 <m.saifi3178@gmail.com>
2019-06-19 22:06:35 +0800

    BaseCommand and SubCommand are different classes now, without any abstract superclass.

--- src/Structures/Command/BaseCommand.ts
+++ src/Structures/Command/BaseCommand.ts
@@ -1,15 +1,64 @@
-import { Command } from './AbstractCommand';
-import { CommandOptions } from 'typings';
-// const chipName = () => __dirname.split(sep).slice(-1)[0];
+import { BaseCommandOptions } from 'typings';
+import { Collection } from 'discord.js';
+import { Context } from '../../Utils/Context';
+import { SubCommand } from './SubCommand';
 
-export class BaseCommand extends Command {
-  public readonly chip: string;
+type Command = SubCommand | BaseCommand;
+
+function validateOptions(options: any): void {
+  if (!options) throw new Error('Invalid CommandOptions provided!');
+  if (!options.name) throw new Error('Invalid command name provided!');
+  if (!options.description) throw new Error('No description provided!');
+  if (!options.chip) throw new Error('Invalid chip name provided!');
+}
+
+function setParent(child: SubCommand, parent: Command): void {
+  if (parent.id === 'base' || parent.id === 'sub') {
+    child.parent = parent;
+  } else {
+    throw new Error(
+      'Parent commands must be instances of BaseCommand or SubCommand!'
+    );
+  }
+}
+
+
+export class BaseCommand {
   public readonly id: 'base';
+  public readonly name: string;
+  public readonly description: string;
+  public readonly usage?: string;
+  public readonly chip: string;
+  public subcommands?: Collection<string, SubCommand>;
 
-  public constructor(options: CommandOptions) {
-    super(options);
-    if (!options.chip) throw new Error('Invalid chip name provided!');
+  public constructor(options: BaseCommandOptions) {
+    validateOptions(options);
+    this.name = options.name;
+    this.description = options.description;
+    this.usage = options.usage;
     this.chip = options.chip;
     this.id = 'base';
+  }
+
+  public async execute(ctx: Context, ...args: string[]): Promise<void> {
+    if (ctx.subcommand) return;
+    if (args.length) throw 'INVALID_ARGS';
+    else throw 'INSUFFICIENT_ARGS';
+  }
+
+  public register(...subCmds: any[]): this {
+    this.subcommands = new Collection();
+    for (const subCmd of subCmds) {
+      if (subCmd.id === 'sub' && subCmd.setParent) {
+        setParent(subCmd, this);
+        this.subcommands.set(subCmd.name, subCmd);
+        continue;
+      } else {
+        throw new Error(
+          'Only instances of the SubCommand class can be registered!'
+        );
+      }
+    }
+    return this;
   }
 }
--- src/Structures/Command/SubCommand.ts
+++ src/Structures/Command/SubCommand.ts
@@ -1,22 +1,73 @@
-import { Command } from './AbstractCommand';
-import { CommandOptions } from 'typings';
+import { Collection } from 'discord.js';
+import { SubCommandOptions } from 'typings';
+import { Context } from '../../Utils/Context';
+import { BaseCommand } from './BaseCommand';
 
-export class SubCommand extends Command {
+type Command = SubCommand | BaseCommand;
+
+function validateOptions(options: any): void {
+  if (!options) throw new Error('Invalid CommandOptions provided!');
+  if (!options.name) throw new Error('Invalid command name provided!');
+  if (!options.description) throw new Error('No description provided!');
+}
+
+function setParent(child: SubCommand, parent: Command): void {
+  if (parent.id === 'base' || parent.id === 'sub') {
+    child.parent = parent;
+  } else {
+    throw new Error(
+      'Parent commands must be instances of BaseCommand or SubCommand!'
+    );
+  }
+}
+
+export class SubCommand {
   public readonly id: 'sub';
+  public readonly name: string;
+  public readonly description: string;
+  public readonly usage?: string;
   public parent?: Command;
+  public subcommands?: Collection<string, SubCommand>;
 
-  public constructor(options: CommandOptions) {
-    super(options);
+  public constructor(options: SubCommandOptions) {
+    validateOptions(options);
+    this.name = options.name;
+    this.description = options.description;
+    this.usage = options.usage;
     this.id = 'sub';
   }
 
-  public setParent(cmd: Command): void {
-    if (cmd instanceof Command) {
-      this.parent = cmd;
-    } else {
-      throw new Error(
-        'Parent commands must be instances of BaseCommand or SubCommand!'
-      );
+  public async execute(ctx: Context, ...args: string[]): Promise<void> {
+    if (ctx.subcommand) return;
+    if (args.length) throw 'INVALID_ARGS';
+    else throw 'INSUFFICIENT_ARGS';
+  }
+
+  public getParent(): Command | undefined {
+    return this.parent;
+  }
+
+  public getBase(): BaseCommand | undefined {
+    let cmd: any = this;
+    while (cmd.parent) {
+      cmd = cmd.parent;
     }
+    return cmd.id === 'base' ? cmd : undefined;
+  }
+
+  public register(...subCmds: any[]): this {
+    this.subcommands = new Collection();
+    for (const subCmd of subCmds) {
+      if (subCmd.id === 'sub' && subCmd.setParent) {
+        setParent(subCmd, this);
+        this.subcommands.set(subCmd.name, subCmd);
+        continue;
+      } else {
+        throw new Error(
+          'Only instances of the SubCommand class can be registered!'
+        );
+      }
+    }
+    return this;
   }
 }
