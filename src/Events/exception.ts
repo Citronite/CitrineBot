0a7c8815342dae1057f392c6d26359940dea1b0d
Quantomistro3178 <m.saifi3178@gmail.com>
2019-06-21 22:14:39 +0800

    Update ignored errors in exception event handler

--- src/Events/exception.ts
+++ src/Events/exception.ts
@@ -5,14 +5,21 @@ import CitrineClient from '../Structures/CitrineClient';
 import QuickEmbed from '../Structures/Utils/QuickEmbed';
 import Context from '../Structures/Utils/Context';
 
-type tCommand = BaseCommand | SubCommand;
+type Command = BaseCommand | SubCommand;
 
-// These errors are not logged to the console
-const ignored = [100, 101, 102, 103, 200, 201, 202];
+/**
+ * These errors are not logged to the console
+ * 101 - MISSING_BOT_PERMS
+ * 102 - MISSING_MEMBER_PERMS
+ * 103 - FAILED_FILTER_CHECKS
+ * 201 - INSIFFICIENT_ARGS
+ * 202 - INVALID_ARGS
+ */
+const ignored = [101, 102, 103, 201, 202];
 
 module.exports = {
   name: 'exception',
-  listener: async (client: CitrineClient, error: Exception, ctx?: Context, cmd?: tCommand) => {
+  listener: async (client: CitrineClient, error: Exception, ctx?: Context, cmd?: Command) => {
     if (ctx) {
       if (cmd && error.code === 201) {
         await ctx.send(QuickEmbed.cmdHelp(ctx, cmd));
@@ -24,7 +31,9 @@ module.exports = {
     }
 
     if (ignored.includes(error.code)) return;
-    client.logger.error(error);
-    client.lastException = error;
+    else {
+      client.logger.error(error);
+      client.lastException = error;
+    }
   }
 };
