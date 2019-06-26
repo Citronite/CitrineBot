4fd83f0b48fec7f07e7a77224622c774037bc249
Quantomistro3178 <m.saifi3178@gmail.com>
2019-06-27 04:55:28 +0800

    Update eval command

--- src/Chips/dev/eval.js
+++ src/Chips/dev/eval.js
@@ -1,27 +1,52 @@
 const { BaseCommand } = require('../../exports');
+const { TOKEN } = require(`${__dirname}/../../../data/core/_instance.json`);
+
+function extractCodeBlock(text) {
+  const rgx = /```(.*?)\n(.*)\n```/s;
+  const result = rgx.exec(text);
+
+  if (!result) return;
+  return {
+    match: result[0],
+    lang: result[1],
+    code: result[2],
+    input: text
+  };
+}
+
+function censor(text, ...words) {
+  for (const word of words) {
+    text = text.replace(word, '<CENSORED>');
+  }
+  return text;
+}
 
 class Eval extends BaseCommand {
   constructor() {
     super({
       name: 'eval',
-      description: 'Evaluate arbitrary JavaScript code.',
+      description: 'Evaluate arbitrary JavaScript code. If used anywhere other than DMs, some text may be censored (such as the bot token)',
       usage: '[p]eval <code>',
       chip: 'dev'
     });
   }
 
-  async execute(ctx, ...args) {
+  async execute(ctx) {
     ctx.lock('botOwner');
 
-    const code = args.join(' ');
+    const extracted = extractCodeBlock(ctx.message.content);
+    if (!extracted) return ctx.error('Failed to parse codeblock!');
+
+    const lang = extracted.lang ? extracted.lang : 'Code';
+    const { code } = extracted;
     try {
-      const result = eval(code);
-      await ctx.send(`[Code]\n${code}\n\n[Result]\n${result}`, {
+      const result = censor(eval(code), TOKEN);
+      return ctx.send(`[${lang}]\n${code}\n\n[Result]\n${result}`, {
         code: 'js',
         split: true
       });
     } catch (err) {
-      await ctx.send(`[Code]\n${code}\n\n[Error]\n${err}`, {
+      return ctx.send(`[${lang}]\n${code}\n\n[Error]\n${err}`, {
         code: 'js',
         split: true
       });
