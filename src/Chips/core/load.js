48f9283c9e740b7132522852bff1d5e73682f20e
Quantomistro3178 <m.saifi3178@gmail.com>
2019-07-09 20:23:17 +0800

    Simplify load/unload commands

--- src/Chips/core/load.js
+++ src/Chips/core/load.js
@@ -10,10 +10,8 @@ class Load extends BaseCommand {
   constructor() {
     super({
       name: 'load',
-      description:
-        'Loads a chip. Separate multiple names with spaces, or enter `all` to load all. ' +
-        'Optionally, add the `--re` flag to hot-reload chips',
-      usage: '[p]load <...chips | "all"> [--re]',
+      description: 'Loads a chip. Separate multiple names with spaces, or enter `all` to load all. ',
+      usage: '[p]load <...chips | "all">',
       chip: 'core'
     });
   }
@@ -22,32 +20,27 @@ class Load extends BaseCommand {
     ctx.lock('botOwner');
     if (!chips.length) throw 'INSUFFICIENT_ARGS';
 
+    const loaded = [];
+    const { loadedChips } = ctx.client.settings;
     const allChips = await readdirAsync(`${root}/bin/Chips`);
     const filteredChips = chips.includes('all')
       ? allChips
       : chips.filter(name => allChips.includes(name));
-    const reload = chips.includes('--re');
-    const loaded = [];
-    const failed = [];
 
     for (const chip of filteredChips) {
       try {
-        if (reload) await ctx.client.clearChipCache(chip);
+        if (loadedChips.includes(chip)) continue;
         await ctx.client.loadChip(chip);
         loaded.push(chip);
       } catch (err) {
-        failed.push(chip);
+        ctx.client.logger.error(err);
       }
     }
 
     const { inline } = ctx.client.utils.format;
-
-    if (failed.length) {
-      await ctx.error(`Failed to load the following chip(s):\n${inline(failed).join(', ')}`);
-    }
     if (loaded.length) {
       return ctx.success(
-        `Successfully ${reload ? 're' : ''}loaded chip(s):\n${inline(loaded).join(', ')}`
+        `Successfully loaded chip(s):\n${inline(loaded).join(', ')}`
       );
     } else {
       return ctx.error('No chips were loaded. Are you sure you provided the correct name(s)?');
---
+++ src/Chips/core/reload.js
@@ -0,0 +1,47 @@
+const { BaseCommand } = require('../../exports');
+
+class Reload extends BaseCommand {
+  constructor() {
+    super({
+      name: 'reload',
+      description: 
+        'Reloads a chip. Separate multiple names with spaces, or enter `all` to reload all. ' +
+        'This command will only work for loaded chips.',
+      usage: '[p]reload <...chips | "all">',
+      chip: 'core'
+    });
+  }
+
+  async execute(ctx, ...chips) {
+    ctx.lock('botOwner');
+    if (!chips.length) throw 'INSUFFICIENT_ARGS';
+
+    const reloaded = [];
+    const { loadedChips } = ctx.client.settings;
+    const filteredChips = chips.includes('all')
+      ? loadedChips
+      : chips.filter(name => loadedChips.includes(name));
+
+    for (const chip of filteredChips) {
+      try {
+        await ctx.client.clearChipCache(chip);
+        await ctx.client.unloadChip(chip);
+        await ctx.client.loadChip(chip);
+        reloaded.push(chip);
+      } catch (err) {
+        ctx.client.logger.error(err);
+      }
+    }
+
+    if (reloaded.length) {
+      const { inline } = ctx.client.utils.format;
+      return ctx.success(
+        `Successfully reloaded chip(s):\n${inline(reloaded).join(', ')}`
+      );
+    } else {
+      return ctx.error('No chips were reloaded. Are you sure you provided the correct name(s)?');
+    }
+  }
+}
+
+module.exports = new Reload();
