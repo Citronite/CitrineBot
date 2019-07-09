666e713ee60df26b3187a6ac0366f85a41c7b1ac
Quantomistro3178 <m.saifi3178@gmail.com>
2019-07-09 20:52:19 +0800

    Update format for _meta.js

--- src/Chips/core/_meta.js
+++ src/Chips/core/_meta.js
@@ -1,5 +1,7 @@
 module.exports = {
   author: 'PandaHappy 🍂#8851',
   description: 'The most important chip for the bot. Contains core functionality.',
-  link: 'https://github.com/Citronite/CitrineBot'
+  link: 'https://github.com/Citronite/CitrineBot',
+  thumbnail: 'https://i.imgur.com/P6DG83M.png',
+  color: 0x9dffbe
 };
